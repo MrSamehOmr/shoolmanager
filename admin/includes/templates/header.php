0713ada8a89ba46fd5bf3f2f5eeb9ad218b25063
Rami Gamal <rami.gamal.mahmoud@gmail.com>
2020-06-30 23:12:36 +0200

    formatting

--- admin/includes/templates/header.php
+++ admin/includes/templates/header.php
@@ -6,4 +6,3 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../general/css/font-awesome.min.css">
     <link rel="stylesheet" href="../admin/layout/css/style.css">
-    <script src="../general/js/jquery-3.5.1.min.js"></script>
